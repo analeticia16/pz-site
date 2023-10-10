e94673e16c968f395dc45ca90cd796bbade2275f
Alexandre Henrique Landgraf <alexandrehlandgraf@gmail.com>
2023-10-10 16:30:54 -0300

    página de serviços, v1

--- src/app/servicos/page.tsx
+++ src/app/servicos/page.tsx
@@ -3,26 +3,46 @@ import React from "react";
 export default function Servicos() {
   return (
     <>
-      <div className="bg-[aspect-ratio:3/2] mb-2 h-72 w-full rounded-sm border bg-[url('https://images.pexels.com/photos/4427431/pexels-photo-4427431.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-cover bg-no-repeat p-8">
-        <h1 className="mb-4 pr-72 text-5xl text-gray-900">
-          Conheça nossos principais serviços jurídico
+      <div className="bg-[aspect-ratio:3/2] mb-2 mt-6 h-96 w-full rounded-lg bg-gray-800/70 bg-[url('https://images.pexels.com/photos/3184465/pexels-photo-3184465.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-cover bg-no-repeat p-8 bg-blend-multiply">
+        <h1 className="mb-4 pr-64 text-6xl text-gray-200">
+          Conheça nossos principais serviços jurídicos
         </h1>
-        <p className="mb-12">E veja como podemos te ajudar</p>
-        <button className="">Contato</button>
+        <p className="mb-20 mt-8 text-2xl text-gray-200">
+          E veja como podemos te ajudar.
+        </p>
+        <a className="rounded bg-gradient-to-r from-primary-900 to-primary-950 px-8 py-4 text-center font-medium text-gray-100 hover:bg-gradient-to-r hover:from-primary-700 hover:to-primary-900">
+          Contato
+        </a>
       </div>
-      <div className="grid grid-cols-2 gap-2">
-        <div className="col-span-1 h-64 rounded-sm border p-8">
+      <div className="mt-6 grid grid-cols-2 gap-2">
+        <div className="col-span-1 h-64 rounded-md border p-8">
           <p className="mb-4">Direito Trabalho</p>
           <h2 className="mb-12 pr-56 text-3xl text-gray-900">
             Correção do seu FGTS
           </h2>
           <button className="">Veja como</button>
         </div>
-        <div className="col-span-1 h-64 rounded-sm border p-8"></div>
-        <div className="col-span-1 h-64 rounded-sm border p-8"></div>
-        <div className="col-span-1 h-64 rounded-sm border p-8"></div>
-        <div className="col-span-1 h-64 rounded-sm border p-8"></div>
-        <div className="col-span-1 h-64 rounded-sm border p-8"></div>
+        <div className="col-span-1 h-64 rounded-md border p-8">
+          <p className="mb-4">Direito Trabalho</p>
+          <h2 className="mb-12 pr-56 text-3xl text-gray-900">
+            Correção do seu FGTS
+          </h2>
+          <button className="">Veja como</button>
+        </div>
+        <div className="col-span-1 h-64 rounded-md border p-8">
+          <p className="mb-4">Direito Trabalho</p>
+          <h2 className="mb-12 pr-56 text-3xl text-gray-900">
+            Correção do seu FGTS
+          </h2>
+          <button className="">Veja como</button>
+        </div>
+        <div className="col-span-1 h-64 rounded-md border p-8">
+          <p className="mb-4">Direito Trabalho</p>
+          <h2 className="mb-12 pr-56 text-3xl text-gray-900">
+            Correção do seu FGTS
+          </h2>
+          <button className="">Veja como</button>
+        </div>
       </div>
     </>
   );
